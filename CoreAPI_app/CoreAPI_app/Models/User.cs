33fa3e27d3a5960f2b22c06d31e11d845025c548
dudyara <58880905+dudyara@users.noreply.github.com>
2021-04-15 22:16:20 +0700

    commit

---
+++ CoreAPI_app/CoreAPI_app/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace CoreAPI_app.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Укажите имя пользователя")]
+        public string Name { get; set; }
+        [Range(1, 100, ErrorMessage = "Возраст должен быть в промежутке от 1 до 100")]
+        [Required(ErrorMessage = "Укажите возраст пользователя")]
+        public int Age { get; set; }
+    }
+}
---
+++ CoreAPI_app/CoreAPI_app/Startup.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using CoreAPI_app.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace CoreAPI_app
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            string con = "Server=(localdb)\\mssqllocaldb;Database=usersdbstore;Trusted_Connection=True;";
+            // устанавливаем контекст данных
+            services.AddDbContext<UsersContext>(options => options.UseSqlServer(con));
+
+            services.AddControllers(); // используем контроллеры без представлений
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseDeveloperExceptionPage();
+
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
